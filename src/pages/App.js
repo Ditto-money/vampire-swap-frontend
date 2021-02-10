4ce2a7183ff75b4e6a04c842c7da017834bdbff7
Bryan Mutai <mutaiwork@gmail.com>
2021-02-10 12:31:40 +0300

    remove dashboard related files/content/

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
-import {
-  BrowserRouter as Router,
-  useLocation,
-} from "react-router-dom";
 
 import Header from 'components/Header';
 import ConnectWallet from 'components/ConnectWallet';
-import Navigation from 'components/Navigation'
-
-import RebaseCooldownStat from 'components/stats/RebaseCooldownStat';
-import PriceStat from 'components/stats/PriceStat';
-import SupplyStat from 'components/stats/SupplyStat';
-import RebaseStat from 'components/stats/RebaseStat';
-import PriceTargetStat from 'components/stats/PriceTargetStat';
-import MarketCapStat from 'components/stats/MarketCapStat';
-
-import PriceChart from 'components/charts/PriceChart';
-import SupplyChart from 'components/charts/SupplyChart';
-import MarketCapChart from 'components/charts/MarketCapChart';
-
-import Rebases from 'components/Rebases';
 
 const useStyles = makeStyles(theme => {
   return {
@@ -68,7 +50,6 @@ const useStyles = makeStyles(theme => {
 
 export default function App() {
   const classes = useStyles();
-  let location = useLocation();
   const [drawerOpen, setDrawerOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
@@ -76,38 +57,13 @@ export default function App() {
   };
 
   return (
-    <Router>
-      <Box className={classes.container}>
-        <Header className={classes.headerContainer} drawerToggle={handleDrawerToggle} />
-        <aside className={classes.navContainer}>
-          <Navigation drawer={drawerOpen} setDrawer={handleDrawerToggle} />
+    <Box className={classes.container}>
+      <Header className={classes.headerContainer} drawerToggle={handleDrawerToggle} />
+      <aside className={classes.navContainer}>
         </aside>
         <main className={classes.contentContainer}>
-          {location.hash === '#stats' && 
-          <Box className={classes.statsContainer}>
-            <RebaseCooldownStat />
-            <PriceStat />
-            <SupplyStat />
-            <RebaseStat />
-            <PriceTargetStat />
-            <MarketCapStat />
-          </Box> 
-          }
-
-          {location.hash === '#volume' && 
-          <Box className={classes.chartsContainer}>
-            <PriceChart />
-            <SupplyChart />
-            <MarketCapChart />
-          </Box>
-          }
-
-          {location.hash === '#rebase' &&
-            <Rebases />
-          }
-          <ConnectWallet />
-        </main>
-      </Box>
-    </Router>
+      </main>
+      <ConnectWallet />
+    </Box>
   );
 }
